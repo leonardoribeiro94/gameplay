e01240fb72a4a6e13ba09df5df4c09a059172ae2
leonardo.silva <leonardo.silva@ltm.digital>
2022-04-07 00:51:38 -0300

    feat: cria front de login

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    Image,
+    StatusBar
+} from 'react-native';
+
+import IllustrationImg from '../../assets/illustration.png';
+import { styles } from './styles';
+import { ButtonIcon } from '../../components/buttonIcon';
+
+
+export function SignIn() {
+
+    return (
+        <View style={styles.container}>
+            <Image
+                source={IllustrationImg}
+                style={styles.image}
+                resizeMode="stretch"
+            />
+
+            <View style={styles.content}>
+                <Text style={styles.title}>a
+                    Conecte-se {'\n'}
+                    Organize {'\n'}
+                    suas jogatinas
+                </Text>
+                <Text style={styles.subtitle}>
+                    Crie grupos para jogar seus games {'\n'}
+                    favoritos com seus amigos
+                </Text>
+
+                <ButtonIcon
+                    title="Entrar com Discord"
+                    activeOpacity={1}
+                />
+            </View>
+        </View>
+    );
+}
